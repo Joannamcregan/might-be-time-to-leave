d9c0c184f41dd91b0b554dc8755be8c87fdef57b
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-09-20 19:10:33 -0400

    tmc

--- generateHtml.js
+++ generateHtml.js
@@ -1,1 +1,62 @@
-console.log('generating');
+import React from "react"
+import ReactDOMServer from "react-dom/server"
+import fs from "fs"
+import Footer from "./app/components/Footer"
+import Header from "./app/components/Header"
+// import LoadingDotsIcon from "./app/components/LoadingDotsIcon"
+import { StaticRouter as Router } from "react-router"
+import StateContext from "./app/StateContext"
+
+function Shell() {
+  return (
+    <StateContext.Provider value={{ loggedIn: false }}>
+      <Router>
+        <Header staticEmpty={true} />
+        {/* <div className="py-5 my-5 text-center">
+          <LoadingDotsIcon />
+        </div> */}
+        <Footer />
+      </Router>
+    </StateContext.Provider>
+  )
+}
+
+const startOfHTML = `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>OurApp</title>
+      
+      <link rel="stylesheet" href="/main.css" />
+    </head>
+    <body>
+      <div id="app">`
+
+const endOfHTML = `</div>
+    </body>
+  </html>`
+
+/* Use Node tools (outside the scope of this course) to setup a
+  stream we can write to that saves to a file on our hard drive
+*/
+const fileName = "./app/index-template.html"
+const writeStream = fs.createWriteStream(fileName)
+
+// Add the start of our HTML template to the stream
+writeStream.write(startOfHTML)
+
+/*
+  Add the actual React generated HTML to the stream.
+  We can use ReactDomServer (you can see how we imported
+  that at the very top of this file) to generate a string
+  of HTML text that a Node stream can leverage.
+*/
+const myStream = ReactDOMServer.renderToPipeableStream(<Shell />, {
+  onAllReady() {
+    myStream.pipe(writeStream)
+    // End the stream with the final bit of our HTML
+    writeStream.end(endOfHTML)
+  }
+})
